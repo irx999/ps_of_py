5fa4fbc13e50148c0b97c5953f694d97188d31df
irx999 <irx999@qq.com>
2025-04-26 15:23:40 +0800

    refactor: 重构代码并添加新功能
    - 重构了 main.py, src/load_data.py 和 src/ps_core.py 的代码结构
    - 添加了新的错误处理和日志记录机制
    - 新增了 task_run 方法实现批量处理功能
    - 优化了层属性修改逻辑，提高了代码复用性
    - 移除了冗余的 ps_layer_changer.py 文件

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-""" 主启动函数 """
+"""主启动函数"""
 
-from src.ps_core import Photoshop
+import win32api
+
 from src.load_data import LoadData
+from src.ps_core import Photoshop
 
 
 def main():
-    """ 主启动函数 """
+    """主启动函数"""
+    try:
+        lodadata = LoadData()
+        ps = Photoshop(*lodadata.settings)
+        # 遍历整个字典
+        for export_name, input_data in lodadata.selected_skus().items():
+            ps.core(export_name, input_data)
+    except Exception as e:
+        print(e)
+        win32api.MessageBox(0, str(e), "错误")
 
-    lodadata  = LoadData()
-    ps = Photoshop(*lodadata.settings)
-    text_data = lodadata.selected_skus()
-    print(text_data)
-    # 遍历整个字典
-    if text_data:
-        for export_name, v in text_data.items():
-            ps.core(export_name=str(export_name).replace(".0",""),
-                input_data = v
-            )
 
 def test():
-    """ 测试专用函数 """
+    """测试专用函数"""
     # ld = LoadData(sheet_name="Sheet1",table_name="表1")
     # print(ld)
-    ps = Photoshop(psd_name="MB")
-    print(ps.layer_outermost_set_name)
+    ps = Photoshop(psd_name="测试专用")
+    print("当前PSD具有以下图层\n", ps.layer_outermost_set_name)
 
-if __name__ == '__main__':
-    main()
+
+def test2():
+    lodadata = LoadData()
+    ps = Photoshop(*lodadata.settings)
+    # 遍历整个字典
+    print(lodadata.selected_skus())
+    error_list = {}
+    task_list = lodadata.selected_skus().items()
+
+    ans = ps.task_run(task_list)
+    enerror_list = "\n".join([f"{key}:{value}" for key, value in error_list.items()])
+    enerror_sum = f" {len(error_list)} / {len(lodadata.selected_skus())}\n"
+    if enerror_list:
+        win32api.MessageBox(0, enerror_sum + enerror_list, "错误")
+
+
+if __name__ == "__main__":
+    test2()
+    pass
